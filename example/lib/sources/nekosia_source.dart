dce4188d74f7e5853d834b4e1872c9c16b32f1f8
iota9star <iota9star@foxmail.com>
2026-01-17 16:33:51 +0800

    🔧 fix: remove unused flutter foundation import
    Remove unused 'package:flutter/foundation.dart' import from nekosia_source.dart.
    The kIsWeb usage is commented out, making this import unnecessary.

--- example/lib/sources/nekosia_source.dart
+++ example/lib/sources/nekosia_source.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:example/exceptions.dart';
 import 'package:example/models/image_post.dart';
