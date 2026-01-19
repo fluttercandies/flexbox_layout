eee3786b31456dbcf75fda15973b31d7983a2310
iota9star <iota9star@foxmail.com>
2026-01-19 13:16:33 +0800

    🔧 chore(example): change default image source to Nekosia
    Update default image source from Konachan to Nekosia in the
    example application configuration.

--- example/lib/config/image_config.dart
+++ example/lib/config/image_config.dart
@@ -1,6 +1,6 @@
 import 'package:example/models/image_post.dart';
 
 class ImageConfig {
-  static SourceType currentSource = SourceType.konachan;
+  static SourceType currentSource = SourceType.nekosia;
   static const int defaultLimit = 20;
 }
